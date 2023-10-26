c64f86b5c919983a0b92bdb6e5aa9ebc4c14b8ad
ozdemirhazal <shazalozdemir@gmail.com>
2023-10-27 01:12:32 +0200

    logos import

--- src/components/DesktopLogoSlider.jsx
+++ src/components/DesktopLogoSlider.jsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
 import "../style/DesktopLogoSlider.css";
+import AudiLogo from "../library/logos/audilogo.png";
+import BmwLogo from "../library/logos/bmwlogo.png";
+import FordLogo from "../library/logos/fordlogo.png";
+import PegoLogo from "../library/logos/pegologo.png";
+import MercedesLogo from "../library/logos/mercedeslogo.png";
+import KiaLogo from "../library/logos/kialogo.png";
+import RenaultLogo from "../library/logos/renaultlogo.png";
+import VwLogo from "../library/logos/vwlogo.png";
 
 const logos = [
-  { id: 1, src: "./src/library/logos/audilogo.png" },
-  { id: 2, src: "./src/library/logos/bmwlogo.png" },
-  { id: 3, src: "./src/library/logos/fordlogo.png" },
-  { id: 4, src: "./src/library/logos/pegologo.png" },
-  { id: 5, src: "./src/library/logos/mercedeslogo.png" },
-  { id: 6, src: "./src/library/logos/kialogo.png" },
-  { id: 7, src: "./src/library/logos/renaultlogo.png" },
-  { id: 8, src: "./src/library/logos/vwlogo.png" },
+  { id: 1, src: AudiLogo },
+  { id: 2, src: BmwLogo },
+  { id: 3, src: FordLogo },
+  { id: 4, src: PegoLogo },
+  { id: 5, src: MercedesLogo },
+  { id: 6, src: KiaLogo },
+  { id: 7, src: RenaultLogo },
+  { id: 8, src: VwLogo },
 ];
 
 const DesktopLogoSlider = () => {
