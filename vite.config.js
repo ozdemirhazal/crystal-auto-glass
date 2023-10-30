532efe42ca8b3c37b6b8636b4e33eaa2f4cd0558
ozdemirhazal <shazalozdemir@gmail.com>
2023-10-30 22:57:19 +0100

    deneme

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
+    base: "/",
   };
 
-  if (command === "build") {
-    config.base = "/"; // Özel etki alanı olmadan kök dizin
+  if (command !== "serve") {
+    config.base = "/crystalautoglass.de/";
   }
 
   return config;
