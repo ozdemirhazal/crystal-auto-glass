be22912d3e59b4669f77847016426cfb46930633
ozdemirhazal <shazalozdemir@gmail.com>
2023-10-30 23:08:57 +0100

    return old

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-// vite.config.js
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // Web sitenizin kök dizini, geliştirme sırasında genellikle "/"" olarak ayarlanır.
-  build: {
-    outDir: "dist", // Derlenen dosyaların çıkış dizini
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/";
+  }
+
+  return config;
 });
